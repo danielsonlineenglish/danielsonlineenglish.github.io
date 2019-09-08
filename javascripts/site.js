3b650e842b159c0e59f8009cc669ae1c04652469
danielsonlineenglish <43343597+danielsonlineenglish@users.noreply.github.com>
2019-09-08 20:01:58 +0100

    Add js to display cookies plugin

---
+++ javascripts/site.js
@@ -0,0 +1,17 @@
+(function ($) {
+  $.cookieBubble({
+    messageText: 'We use cookies to personalize your experience. By continuing to visit this website you agree to our use of cookies.',
+    messageTextColor:'#000',
+    messageFontSize:'17px',
+    buttonText:'Ok',
+    buttonColor:'#ffa500',
+    buttonFontSize:'15px',
+    iconColor:'#ffa500',
+    iconVisibility:false,
+    cookiePolicyButtonText:'My cookie Policy Text',
+    cookiePolicyButtonTextColor:'gray',
+    cookiePolicyButtonUrl:'http://google.com',
+    cookiePolicyButtonTarget:'_self',
+    boxAppearDelay:500
+  });
+})(jQuery);
